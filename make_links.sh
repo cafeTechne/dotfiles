7c5f23f120b0ad922f7fe3f8545288ecebe663c4
jandersson <jonas@andersson.io>
2017-02-15 15:36:32 +0100

    force symlink creation; symlink .bashrc

--- make_links.sh
+++ make_links.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+echo Setting .bashrc
+ln -sf $PWD/bash/.bashrc $HOME/.bashrc
+
 echo Getting vundle
 git submodule update --init
 
 echo Making symbolic links
-ln -s $PWD/.vim $HOME/.vim
-ln -s $PWD/.vim/.vimrc $HOME/.vimrc
+ln -sf $PWD/.vim $HOME/.vim
+ln -sf $PWD/.vim/.vimrc $HOME/.vimrc
 
 echo Installing bundles
 vim +BundleInstall +qall
